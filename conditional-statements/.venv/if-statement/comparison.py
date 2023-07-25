217ad57a12d46df9816e0b12225abd9317549e38
ashertopaz <asheranumah@gmail.com>
2023-07-25 19:32:01 +0100

    comparison opeators

---
+++ conditional-statements/.venv/if-statement/comparison.py
@@ -0,0 +1,18 @@
+x = 10
+if x==10:
+    print('Equals 10')
+if x > 4:
+    print('Greater than 4')
+
+if x >=5 :
+    print('Greater than or Equals 5')
+
+if x < 6:
+    print('Less than or equals 6')
+
+if x<=10:
+    print('Less than or equal 10')
+if x!=10:
+    print('Not equal 10')
+    
+    
