4fbabb3aadf452682e885f6312e6c8a921291a53
ashertopaz <asheranumah@gmail.com>
2023-07-27 22:57:24 +0100

    calculated gross payment
    for hours less than 40 and more than 40

---
+++ conditional-statements/.venv/assignment/gross-pay.py
@@ -0,0 +1,26 @@
+hrs = input('Enter number of hours:')
+rph = input('Enter rate per hour:')
+
+h=float(hrs)
+rh=float(rph)
+
+
+if h <= 40:
+    gp=rh*h
+    print('Gross pay is:',gp)
+
+elif h > 40:
+    ogp=rh*h
+    egp=(h-40) * (rh*0.5)
+    gp = ogp + egp
+    print('Gross pay is:',gp)
+print('Done')
+
+
+
+
+
+
+
+
+
