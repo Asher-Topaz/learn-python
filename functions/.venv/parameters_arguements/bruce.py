7071bd9dddf2d32ed780960b1278168c5b2a24e5
ashertopaz <asheranumah@gmail.com>
2023-08-01 11:33:55 +0100

    created function with parameter bruce
    passed arguement spam to variable bruce

---
+++ functions/.venv/parameters_arguements/bruce.py
@@ -0,0 +1,7 @@
+#function defined with variable parameter bruce
+def print_twice(bruce):
+    print(bruce)
+    print(bruce)
+
+#function call with arguement name spam assigned to bruce
+print_twice('spam')
