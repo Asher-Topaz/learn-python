e2e193fef77fd3df920bfb1263199ae4a54d8dfc
ashertopaz <asheranumah@gmail.com>
2023-08-26 11:29:00 +0100

    for statement example

---
+++ Iterations/.venv/example4.py
@@ -0,0 +1,4 @@
+friends = ['Joseph', 'Glenn', 'Sally'] #list of three strings in a set
+for friend in friends: #for statement that assigns friend to strings in the string
+    print('Happy New Year:',friend)
+print('Done!')
