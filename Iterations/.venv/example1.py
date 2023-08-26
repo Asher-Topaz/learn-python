61a6ac534173131f88f21eb6a77daf6e04563bb7
ashertopaz <asheranumah@gmail.com>
2023-08-26 10:04:06 +0100

    new python venv

--- Iterations/.venv/example1.py
+++ Iterations/.venv/example1.py
@@ -1,5 +1,5 @@
 n=5 #initial value of n is 5
 while n > 0: #loop works as long as n > 0
-    print(n)
+    print(n) #the value of n is printed so far n is > 1
     n = n-1 #decrement of n by 1
 print('Blastoff!')
