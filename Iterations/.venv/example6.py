a7da883bb78f9c9e96db744227c0e62d166bb2a6
ashertopaz <asheranumah@gmail.com>
2023-08-28 11:02:49 +0100

    added the total value of list of numbers using for loop

---
+++ Iterations/.venv/example6.py
@@ -0,0 +1,5 @@
+total=0 #varaible declaration befor loop starts
+
+for itervar in [3,4,12,9,74,10,20]: #iteration variable that countrs through list of numbers
+    total=total + itervar
+print('Total:',total)
