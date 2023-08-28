b55a57e5b2129078a7a3dff9503395c44a08ba7c
ashertopaz <asheranumah@gmail.com>
2023-08-28 10:48:34 +0100

    used for loop to count the list of numbers

---
+++ Iterations/.venv/example5.py
@@ -0,0 +1,4 @@
+count = 0 #initialized variable before start of loop
+for itervar in [3,41,12,9,74,15]: #itervar is iteration variable
+    count = count + 1 #variable that counts number of times to iterate through the list
+print('Count: ',count) #prints 
