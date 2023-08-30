4244290637547c69824e248b9b73f6fb9230b8f7
ashertopaz <asheranumah@gmail.com>
2023-08-30 18:19:19 +0100

    slice a segment of string and print the results

---
+++ strings/.venv/example5.py
@@ -0,0 +1,8 @@
+#slicing strings
+s = 'Monty Python'
+#selecting a slic of a string using [n:m] operator
+#select the first character but not the last
+print (s[0:5])#select characters between index 0 and 4
+print(s[6:12])#select characters between index 6 and 11
+
+
