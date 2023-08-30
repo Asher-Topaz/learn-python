3b077fa56bf13cb8dbc0686139e096aedc676831
ashertopaz <asheranumah@gmail.com>
2023-08-30 11:10:01 +0100

    used while loop to traverse through string
    printed each character in the string

---
+++ strings/.venv/example3.py
@@ -0,0 +1,12 @@
+#traversal through a string kl2```````` 111111111
+fruit = 'banana'
+#beginning of while loop
+
+index=0   
+while index < len(fruit): #condition for loop to keep running
+    #assigning charcter in a sequence to variable letter
+    letter = fruit[index]
+    print(letter)#print each iteration value of variable letter
+    index=index + 1
+
+
