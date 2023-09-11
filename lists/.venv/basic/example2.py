df0634208c750f2192752ad610e442cf9d5aeb0d
ashertopaz <asheranumah@gmail.com>
2023-09-11 14:08:45 +0100

    used append function to add items to list
    accessed items in a list using braces and expression

---
+++ lists/.venv/basic/example2.py
@@ -0,0 +1,11 @@
+mylist = []
+#append() function adds an element to the end of a list
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+#access list by using braces and expression
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(mylist)
+
