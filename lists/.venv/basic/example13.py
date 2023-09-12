b382c2d4f327c2d65fdde24dbc59c3f648a61a62
ashertopaz <asheranumah@gmail.com>
2023-09-12 13:10:37 +0100

    more examples

---
+++ lists/.venv/basic/example13.py
@@ -0,0 +1,18 @@
+#creating a list
+
+list = []
+print("Blank List: ")
+print(list)
+
+#creating a list of numbers
+list = [10,20,14]
+print("\nList of numbers: ")
+print(list)
+
+#creating a list of strings and accessing
+#using index
+list = ['Genius', 'Prodigy', 'Brains','Brilliant']
+print("\n List Items: ")
+print(list[0])
+print(list[2])
+
