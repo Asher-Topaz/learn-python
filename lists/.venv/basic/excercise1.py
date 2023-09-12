7840929b5223c1ac05bdfb8efd489f76d7926dd1
ashertopaz <asheranumah@gmail.com>
2023-09-12 07:22:03 +0100

    used append method to add items to list

---
+++ lists/.venv/basic/excercise1.py
@@ -0,0 +1,17 @@
+numbers = []
+strings = []
+names = ['John', 'Eric', 'Asher']
+
+numbers.append(1)  
+numbers.append(2)
+numbers.append(3)
+
+strings.append('hello')
+strings.append('world')
+
+second_name = names[1]
+
+print(numbers)
+print(strings)
+print("The second name on the name list is %s" % second_name)
+
