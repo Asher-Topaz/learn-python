3ad767b0e115a3b2b0c11f943810a9dbfeea2941
ashertopaz <asheranumah@gmail.com>
2023-09-12 08:25:33 +0100

    accessed items in a list

---
+++ lists/.venv/basic/example7.py
@@ -0,0 +1,14 @@
+spam = ['dog', 'cat', 'bat', 'rat']
+
+print(spam[0])
+
+print(spam[1])
+
+print(spam[2])
+
+print(spam[3])
+
+print('Hello, '   +spam[0])
+
+print("The " +spam[0]  + " ate the " + spam[1])
+
