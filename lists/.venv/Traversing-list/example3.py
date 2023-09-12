434040628a394790bd266600da6682c17b18b5d4
ashertopaz <asheranumah@gmail.com>
2023-09-12 16:33:16 +0100

    used range funcion and for loop to taverse list

---
+++ lists/.venv/Traversing-list/example3.py
@@ -0,0 +1,5 @@
+supplies = ['pens','staplers','flamethrowers', 'binders']
+
+#use range function to iterate over list items
+for i in range(len(supplies)):
+    print('Index ' + str(i) + ' in supplies is: ' + supplies[i] )
