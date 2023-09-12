d9c32741dc201a457518f60d74478e59447b6bd0
ashertopaz <asheranumah@gmail.com>
2023-09-12 07:57:08 +0100

    updated list

---
+++ lists/.venv/basic/example5.py
@@ -0,0 +1,10 @@
+#list are mutable and can be updated
+
+list = ['math', 'phyics','chemistry',2000,2010]
+print('Value at index 2 of list')
+print(list[2])
+
+print('New value at index 2 of list')
+list[2] = 'Biology'
+print(list[2])
+
