821897bbc1924cb6a723972964150748f3ed8b3d
ashertopaz <asheranumah@gmail.com>
2023-09-12 09:47:01 +0100

    changed value of list

---
+++ lists/.venv/basic/example10.py
@@ -0,0 +1,11 @@
+#changing values in a list with indexes
+
+spam = ['house', 'car', 'Plane','Jet']
+print(spam)
+spam[1] = 'penthouse'
+print(spam[1])
+
+spam[3] = spam[1]
+print(spam[3])
+
+print(spam)
