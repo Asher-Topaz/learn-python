1376aae400070ff77ef5976cd7be12c5589c8ca7
ashertopaz <asheranumah@gmail.com>
2023-09-12 13:19:43 +0100

    More examples

---
+++ lists/.venv/basic/example14.py
@@ -0,0 +1,16 @@
+#creating a List with
+#the use of Numbers
+#Having duplicate values
+
+List = [1,2,4,4,3,3,3,6,5]
+print('\nList with the use of Numbers: ')
+print(List)
+
+#Creating a List with
+#mixed type of values
+#Having number and Strings
+
+List = [1,2,"Super",5,"human","intelligence"]
+print("\nList with the use of mixed values:")
+print(List)
+
