900d100c09bb9a2d4a082091549c2cb4a798cc57
ashertopaz <asheranumah@gmail.com>
2023-09-12 13:26:16 +0100

    more examples

---
+++ lists/.venv/basic/example15.py
@@ -0,0 +1,13 @@
+#creating a multi-dimensional List
+#By nestin list inside of a list
+
+List = [['House', 'Money','Jet'], ['Travel']]
+
+#accessing an element from the 
+#Multi-Dimensional List using
+#index number
+
+print("Accessing an element from multi-dimensional list")
+print(List[0][1])
+print(List[1][0])
+
