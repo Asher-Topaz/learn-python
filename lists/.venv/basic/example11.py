53e94092e257d8bc245eaf036db2b122b2889fdd
ashertopaz <asheranumah@gmail.com>
2023-09-12 11:10:47 +0100

    concatenated list and duplicated lists

---
+++ lists/.venv/basic/example11.py
@@ -0,0 +1,15 @@
+#concatenating list and list replication
+
+numbers = [1,2,3,4,5]
+letters = ['A','B','C','D']
+
+print(numbers)
+print(letters)
+ 
+
+numbers = numbers + letters
+print(numbers)
+
+print(letters * 3)
+
+
