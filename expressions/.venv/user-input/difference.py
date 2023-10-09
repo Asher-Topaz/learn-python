9245754da85f4fc68fbcbecf8af2ebc9cc893de9
ashertopaz <asheranumah@gmail.com>
2023-10-09 10:34:01 +0100

    difference between user input

---
+++ expressions/.venv/user-input/difference.py
@@ -0,0 +1,10 @@
+#program to find difference between user input
+
+#ask user to input integers
+number1 = int(input('Enter first integer  '))
+number2 = int(input('Enter second integer  '))
+
+diff = (number1 - number2)
+
+print('The diff between two numbers is',diff)
+
