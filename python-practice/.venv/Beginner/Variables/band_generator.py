9b37a9239bcfad8176cb16215e71943cb6c4e91d
ashertopaz <asheranumah@gmail.com>
2024-01-11 18:08:30 -0500

    band name from user input

---
+++ python-practice/.venv/Beginner/Variables/band_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band generator")
+inp1 = input("What city did you grow up in  ")
+inp2 = input("What is the name of your pet?  ")
+print("Your band name is " + inp1 + " " + inp2)
