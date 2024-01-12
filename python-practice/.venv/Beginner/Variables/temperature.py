8edc4012b627f700dcdc9503b9d16d67cd7b4623
ashertopaz <asheranumah@gmail.com>
2024-01-12 08:29:02 -0500

    temperature conversion

---
+++ python-practice/.venv/Beginner/Variables/temperature.py
@@ -0,0 +1,6 @@
+temp_in_celcius = float(input("Enter a temperature in celsius"))
+temp_in_fahrenheint = (temp_in_celcius * 9/5) + 32
+
+print("Temperature in fahrenheint is: ",temp_in_fahrenheint)
+
+
