6f731ebdd37a3f23f57e2dc4aa35e265f540e3f9
ashertopaz <asheranumah@gmail.com>
2024-01-12 08:54:29 -0500

    greater than and less than

---
+++ python-practice/.venv/Beginner/conditional_execution/greater_less_than.py
@@ -0,0 +1,11 @@
+x = int(input("Enter a number "))
+print("X is: ", x)
+y = int(input("Enter a second number "))
+print("y is: ", y)
+
+if x > y:
+    print("X is greater than y")
+elif y > x:
+    print("y is greater than x")
+else:
+    print("x and y are equal")
