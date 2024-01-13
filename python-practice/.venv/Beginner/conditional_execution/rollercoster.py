87e470baf83ce92f40c87b21e0b01663b055054d
ashertopaz <asheranumah@gmail.com>
2024-01-13 17:12:25 -0500

    nested loops

--- python-practice/.venv/Beginner/conditional_execution/rollercoster.py
+++ python-practice/.venv/Beginner/conditional_execution/rollercoster.py
@@ -3,6 +3,15 @@ height = int(input("What is your height in Cm? "))
 
 if height > 120:
     print("You can rid the rollercoster")
+    age = int(input("What is your age? "))
+    if age >= 18:
+        print("Your riding fee is $12 ")
+    elif age < 12:
+        print("Your riding fee is $5")
+
+    else:
+        print("Your riding fee is 5$")
+
 else:
     print("Sorry you are too short to ride")
 
