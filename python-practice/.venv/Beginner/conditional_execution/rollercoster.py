813a94473502f6452c5b5e0480847213a4350bb3
ashertopaz <asheranumah@gmail.com>
2024-01-13 16:53:42 -0500

    if and else statement

---
+++ python-practice/.venv/Beginner/conditional_execution/rollercoster.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoster! ")
+height = int(input("What is your height in Cm? "))
+
+if height > 120:
+    print("You can rid the rollercoster")
+else:
+    print("Sorry you are too short to ride")
+
