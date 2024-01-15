5d1c03d956be2224890ceca2f53543f8a423bef8
ashertopaz <asheranumah@gmail.com>
2024-01-15 08:34:06 -0500

    nested lists
    fruits and vegetables
    
    dirty dozen

---
+++ python-practice/.venv/Beginner/random + lists/dirty_dozen.py
@@ -0,0 +1,10 @@
+dirty_dozen = ["Strawberries","Spinanch","Kale","Nectarine","Apples","Peaches","Grapes",
+               "Cherries","Pears","Tomato"]
+
+fruits = ['Strawberries','Nectarine','Apple','Grapes','Peaches','Cherries','Pears']
+vegetables = ['Spinanch','Kale','Tomatoes','Celery','Tomatoes','Potatoes']
+
+dirty_dozen = [fruits,vegetables]
+
+print(dirty_dozen)
+
