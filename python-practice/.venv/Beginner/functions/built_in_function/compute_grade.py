afe23420538f5d982272f1f6f4dfa87c33326ce0
ashertopaz <asheranumah@gmail.com>
2024-01-16 17:39:01 -0500

    function return value

---
+++ python-practice/.venv/Beginner/functions/built_in_function/compute_grade.py
@@ -0,0 +1,20 @@
+def grade(score):
+    if score >= 0.9:
+        return 'A'
+    elif score >= 0.8:
+        return 'B'
+    elif score >= 0.7:
+        return 'C'
+    elif score >= 0.6:
+        return 'D'
+    elif score < 0.6:
+        return 'F'
+    
+
+inp = float(input("Enter your score"))
+
+student_score = grade(inp)
+
+print(student_score)
+
+
