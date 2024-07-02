6d3c3bb60ece10b92874d78005874896092bb3ca
ashertopaz <asheranumah@gmail.com>
2024-07-02 11:37:15 -0400

    functions

---
+++ functions_1/default_aurgument1.py
@@ -0,0 +1,7 @@
+#default arguments
+def myFun(x, y=50):
+    print("x:", x)
+    print("y:", y)
+
+myFun(10)
+
---
+++ functions_1/positional_argument.py
@@ -0,0 +1,12 @@
+def nameAge(name, age):
+    print("Hi, I am",name)
+    print("My age is",age)
+
+#arguement is given in order
+print("Case-1:")
+nameAge("Suraj",27)
+
+print("\nCase-2")
+nameAge(27, "Suraj")
+
+
